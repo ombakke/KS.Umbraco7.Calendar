bb334b0e3514446af838f64d0085034a13cc0a0c
Bjarne Fyrstenborg <bf@oerskov.dk>
2019-02-07 16:08:41 +0100

    Update project

--- src/KS.Umbraco7.Calendar.Core/Properties/AssemblyInfo.cs
+++ src/KS.Umbraco7.Calendar.Core/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kreativ Strek AS")]
 [assembly: AssemblyProduct("KS.Umbraco7.Calendar.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
