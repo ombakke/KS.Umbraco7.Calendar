18f3fd147a84869a6ffcf7f0e38ec1690eb5ae31
Bjarne Fyrstenborg <bf@oerskov.dk>
2019-02-07 23:51:27 +0100

    Update project

--- src/KS.Umbraco7.Calendar.Web/Properties/AssemblyInfo.cs
+++ src/KS.Umbraco7.Calendar.Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KS.Umbraco7.Calendar.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
